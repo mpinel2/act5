29ebe6cf611277f132ff44c6671820f9be13f5a3
mpinel2 <montse.pinel@gmail.com>
2023-02-12 01:23:56 +0100

    creado el componente y la interficie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Noticia } from './interfaces/noticia.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'act5';
+  titulo: string = "";
+  url: string = "";
+  textoNoticia: string = "";
+  fechaPublicacion: string = "";
+
+  arrNoticias: Noticia[] = [];
+
+  guardarNoticia(): void {
+    let noticia: Noticia ={
+      titulo: this.titulo,
+      url: this.url,
+      textoNoticia: this.textoNoticia,
+      fechaPublicacion: this.fechaPublicacion
+    }
+
+    this.arrNoticias.push(noticia);
+
+    this.titulo =""
+    this.url= ""
+    this.textoNoticia= ""
+    this.fechaPublicacion= ""
+
+
+  }
+
 }
---
+++ src/app/interfaces/noticia.interface.ts
@@ -0,0 +1,6 @@
+export interface Noticia {
+    titulo: string;
+    url: string;
+    textoNoticia: string;
+    fechaPublicacion: string;
+}
